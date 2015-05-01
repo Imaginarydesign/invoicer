e5c84496134181614fb7bf87da78eb2d152adddb
Adam Koniuszewski <adam@imaginarydesign.co.uk>
2015-05-01 07:15:56 +0100

    Add delete row

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,7 +4,20 @@ console.log('\'Allo \'Allo!');
 $(document).ready(function() {
 
   'use strict';
+
+  // Add row
+  $('#addrow').click(function(){
+    $('.item-row:last').after('<tr class="item-row"><td><div class="del"><textarea class="form-control" id="date">Website</textarea><a href="#" class="deleterow"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></a></div></td><td><textarea class="form-control" id="date">Description</textarea></td><td><textarea class="form-control" id="date">£1999</textarea></td><td><textarea class="form-control" id="date">1</textarea></td><td><textarea class="form-control" id="date">$1999</textarea></td></tr>');
+  });
   
-  
+  // Delete row
+  $(document).on('click', '.deleterow' , function() {
+    // $(this).parents('.item-row').remove();
+    var killrow = $(this).parents('.item-row');
+    killrow.fadeOut(300, function(){
+      $(this).remove();
+    });
+  });
+
 
 });
